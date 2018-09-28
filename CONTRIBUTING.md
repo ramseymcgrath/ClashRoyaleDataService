7f5a2284b9b3536234776c3e7bf122106242f587
Ramsey McGrath <ramseymcgrath@gmail.com>
2018-09-28 04:11:06 -0400

    Should build now, Fixed readme a bit, fixed deps, testing yet to be done

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,4 +1,8 @@
-# Contributing
+# Contributing to Clash Royale Data Service
+
+This software is available clear and free, do whatever you want
+
+## Base Google Contributing Info
 
 *[Website](https://developers.google.com/web/starter-kit/) related issues should be filed on the [Web Fundamentals](https://github.com/google/WebFundamentals/issues/new) issue tracker.*
 
@@ -6,13 +10,10 @@ Web Starter Kit is an open source project. It is licensed using the
 [Apache Software License 2.0](http://www.apache.org/licenses/LICENSE-2.0.html).
 We appreciate pull requests, here are our guidelines:
 
-1. File a bug at https://github.com/google/web-starter-kit/issues (if there
-isn’t one already). If your patch is going to be large it might be a good idea
-to get the discussion started early. We are happy to discuss it in a new issue beforehand.
-1. Make sure that patches provide justification for why they should be merged. Web Starter Kit is an opinionated project and as such, patches which add support for alternative choices to those we prescribe are not guaranteed to land.
-1. Due to legal reasons, all contributors must sign a contributor license
-agreement, either for an
-[individual](https://code.google.com/legal/individual-cla-v1.0.html) or
-[corporation](https://code.google.com/legal/corporate-cla-v1.0.html), before a
-patch can be accepted.
-1. We ask that you squash all the commits together before pushing.
+1. File a bug at [Bug Tracker](https://github.com/google/web-starter-kit/issues) (if there isn’t one already). If your patch is going to be large it might be a good ideato get the discussion started early. We are happy to discuss it in a new issue beforehand.
+
+2. Make sure that patches provide justification for why they should be merged. Web Starter Kit is an opinionated project and as such, patches which add support for alternative choices to those we prescribe are not guaranteed to land.
+
+3. Due to legal reasons, all contributors must sign a contributor license agreement, either for an [individual](https://code.google.com/legal/individual-cla-v1.0.html) or [corporation](https://code.google.com/legal/corporate-cla-v1.0.html), before a patch can be accepted.
+
+4. We ask that you squash all the commits together before pushing.
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,76 +1,35 @@
-/*!
- *
- *  Web Starter Kit
- *  Copyright 2015 Google Inc. All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *    https://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *
- */
 /* eslint-env browser */
 (function() {
-  'use strict';
+  let isLocalhost = false;
+  isLocalhost = Boolean(window.location.hostname === 'localhost' || window.location.hostname === '[::1]');
 
-  // Check to make sure service workers are supported in the current browser,
-  // and that the current page is accessed from a secure origin. Using a
-  // service worker from an insecure origin will trigger JS console errors. See
-  // http://www.chromium.org/Home/chromium-security/prefer-secure-origins-for-powerful-new-features
-  var isLocalhost = Boolean(window.location.hostname === 'localhost' ||
-      // [::1] is the IPv6 localhost address.
-      window.location.hostname === '[::1]' ||
-      // 127.0.0.1/8 is considered localhost for IPv4.
-      window.location.hostname.match(
-        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-      )
-    );
+  if ('serviceWorker' in navigator && (window.location.protocol === 'https:' || isLocalhost))
 
-  if ('serviceWorker' in navigator &&
-      (window.location.protocol === 'https:' || isLocalhost)) {
-    navigator.serviceWorker.register('service-worker.js')
-    .then(function(registration) {
-      // updatefound is fired if service-worker.js changes.
+    {navigator.serviceWorker.register('service-worker.js').then(function(registration){
       registration.onupdatefound = function() {
-        // updatefound is also fired the very first time the SW is installed,
-        // and there's no need to prompt for a reload at that point.
-        // So check here to see if the page is already controlled,
-        // i.e. whether there's an existing service worker.
-        if (navigator.serviceWorker.controller) {
-          // The updatefound event implies that registration.installing is set:
-          // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
-          var installingWorker = registration.installing;
-
-          installingWorker.onstatechange = function() {
-            switch (installingWorker.state) {
-              case 'installed':
-                // At this point, the old content will have been purged and the
-                // fresh content will have been added to the cache.
-                // It's the perfect time to display a "New content is
-                // available; please refresh." message in the page's interface.
-                break;
-
-              case 'redundant':
-                throw new Error('The installing ' +
-                                'service worker became redundant.');
-
-              default:
-                // Ignore
-            }
+          if (navigator.serviceWorker.controller) {
+            installingWorker = registration.installing;
+            installingWorker.onstatechange = function() {
+              switch (installingWorker.state) {
+                case 'installed':
+                  break;
+                case 'redundant':
+                  throw new Error('The installing service worker became redundant.');
+                default:
+                  // Ignore
+              };
+            };
           };
-        }
       };
-    }).catch(function(e) {
-      console.error('Error during service worker registration:', e);
-    });
-  }
+    }
+  )};
 
-  // Your custom JavaScript goes here
-})();
+});
+(function(playerID) {
+  const {Client} = require('croyale');
+  const client = new Client('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTc1MywiaWRlbiI6IjQyOTg2NjE5OTgzMzI0Nzc0NCIsIm1kIjp7fSwidHMiOjE1Mzc5OTc0NzM1MzJ9.PyYVJUwLgTOD8JSw285uk89CLlnxm67L5mFwfTZLeHU');
+
+  new newclient(playerID);
+  newclient = client.Users.getProfile(playerID, {keys: ['tag', 'name']});
+});
+
